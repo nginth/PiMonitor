bf000d7aea9727e0298fedee039940d5a9bc08d7
Nick Ginther <nginth@gmail.com>
2018-01-09 15:43:06 +0000

    get device from config file

--- monitor.py
+++ monitor.py
@@ -2,12 +2,21 @@ from configparser import ConfigParser
 from clearblade.ClearBladeCore import System
 
 
-def systemFromCfg(filename):
-    config = ConfigParser()
-    config.read(filename)
+def systemFromCfg(config):
     if 'system' not in config:
         raise KeyError('System key not found in clearblade.ini.')
 
     return System(config['system']['key'], config['system']['secret'])
 
-system = systemFromCfg('clearblade.ini')
+
+def deviceFromCfg(config, system):
+    if 'device' not in config:
+        raise KeyError('Device key not found in clearblade.ini.')
+
+    return system.Device(config['device']['name'], config['device']['active_key'])
+
+config = ConfigParser()
+config.read('clearblade.ini')
+system = systemFromCfg(config)
+device = deviceFromCfg(config, system)
+print(device)
