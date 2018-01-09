223290621c67dfd893c52a600b9068141ff2de6c
Nick Ginther <nginth@gmail.com>
2018-01-09 16:28:10 +0000

    publish test message to channel

--- monitor.py
+++ monitor.py
@@ -1,22 +1,38 @@
 from configparser import ConfigParser
 from clearblade.ClearBladeCore import System
+import time
 
 
-def systemFromCfg(config):
+def system_from_cfg(config):
     if 'system' not in config:
         raise KeyError('System key not found in clearblade.ini.')
 
     return System(config['system']['key'], config['system']['secret'])
 
 
-def deviceFromCfg(config, system):
+def device_from_cfg(config, system):
     if 'device' not in config:
         raise KeyError('Device key not found in clearblade.ini.')
 
     return system.Device(config['device']['name'], config['device']['active_key'])
 
+
+def messaging_from_cfg(config, system, device):
+    return system.Messaging(device)
+
+
 config = ConfigParser()
 config.read('clearblade.ini')
-system = systemFromCfg(config)
-device = deviceFromCfg(config, system)
-print(device)
+system = system_from_cfg(config)
+device = device_from_cfg(config, system)
+
+msg_client = messaging_from_cfg(config, system, device)
+
+
+def on_connect(client, userdata, flags, rc):
+    client.publish('/rpi/cpu_usage', 'test')
+
+msg_client.on_connect = on_connect
+msg_client.connect()
+time.sleep(10)
+msg_client.disconnect()
